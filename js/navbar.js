0d5c8f847b56809c0169f2f8a3ce65c83a6d8b52
Kawtar Mesfaouyi <mesfaouyi.kawtar@outlook.fr>
2024-02-19 16:51:05 +0100

    first commit

---
+++ js/navbar.js
@@ -0,0 +1,9 @@
+/*Toggle pour le menu burger en version responsive; je click sur l icone pour afficher la div contenant mon menu*/
+document.addEventListener('DOMContentLoaded', function() {
+    var burgerIcon = document.getElementById('burger'); // je recupere l'icone
+    var navMobile = document.getElementById('menuMobile');// je recupere ma navbar mobile
+
+    burgerIcon.addEventListener('click', function() {// toggle en click sur l'icone
+        navMobile.classList.toggle('show');// rajoute la classe show ou j'ai inclue le css qui me faut pour la version mobile
+    });
+});
