adca6a231532bf9c5f7073cdf4e4ead6b675cb55
Kawtar Mesfaouyi <mesfaouyi.kawtar@outlook.fr>
2024-03-01 17:26:09 +0100

    j17

--- js/music.js
+++ js/music.js
@@ -19,6 +19,11 @@ console.log(images); // c tout les images à changer////////////////////////////
 
 //je prends les musiques par id de leur div
 // pour changer le src de l'audioplaying en fonction de click sur la div 
+//Changement de la cover de la musique 
+//dans la div de la musique playing
+let cover = document.getElementById("coverMusique");
+let title = document.getElementById("TitrePlaying");
+
 
 let musique1 = document.getElementById("musique1");
 let musique2 = document.getElementById("musique2");
@@ -26,24 +31,42 @@ let musique3 = document.getElementById("musique3");
 let musique4 = document.getElementById("musique4");
 let musique5 = document.getElementById("musique5");
 
-console.log(musique1);
+// console.log(musique1);
+
+// au chargement on a pas encore choisir un titre 
+function setDefaultImage() {
+    cover.src = "assets/images/girlListeningMusic.jpg";
+    title.innerHTML = "Decouvrez nos musiques";
+};
+setDefaultImage();
 
 musique1.addEventListener("click", function(){
     audioPlaying.src = musiques[0];
+    cover.src = "assets/images/cd.jpg";
+    title.innerHTML = "Titre 1, Artiste";
+
 })
 
 musique2.addEventListener("click", function(){
     audioPlaying.src = musiques[1];
+    cover.src = "assets/images/musicyellow.jpg";
+    title.innerHTML = "Titre 2 , Artiste";
 })
 
 musique3.addEventListener("click", function(){
     audioPlaying.src = musiques[2];
+    cover.src = "assets/images/casettes.jpg";
+    title.innerHTML = "Titre 3 , Artiste";
 })
 
 musique4.addEventListener("click", function(){
     audioPlaying.src = musiques[3];
+    cover.src ="assets/images/guitar.jpg";
+    title.innerHTML = "Titre 4 , Artiste";
 })
 
 musique5.addEventListener("click", function(){
     audioPlaying.src = musiques[4];
+    cover.src ="assets/images/musicyellow.jpg";
+    title.innerHTML = "Titre 5, Artiste";
 })
